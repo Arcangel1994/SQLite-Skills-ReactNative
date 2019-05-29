da15a01d3dd3b045bef3f410e27fdd86cf943024
Miguel Muñoz Medina <miguel.angel.medina.1994@gmail.com>
2019-05-29 12:51:03 -0500

    Update Firebase.js

--- NiceQuotes/js/Firebase.js
+++ NiceQuotes/js/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 
 const config = {
     apiKey: "",
-    authDomain: "quotes-3118f.firebaseapp.com",
+    authDomain: "",
     databaseURL: "",
-    projectId: "quotes-3118f",
-    storageBucket: "quotes-3118f.appspot.com",
-    messagingSenderId: "970962896256",
-    appId: "1:970962896256:web:09f78571e875c919"
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   export default class Firebase{
